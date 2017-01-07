957493527c5cf81c194b52e385ce0d1b763b50f3
rnoxiv <sabatier.david.93@gmail.com>
2017-01-07 21:54:49 +0100

    added Enemies + particles

---
+++ Assets/Scripts/Enemies/EnemyMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour {
+
+    public float speed = 20f;
+    public Transform[] path;
+
+    private int nextPoint = 1;
+	// Use this for initialization
+	void Start () {
+        transform.position = path[0].position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position == path[nextPoint].position)
+        {
+            nextPoint++;
+            if (nextPoint >= path.Length)
+                nextPoint = 0;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, path[nextPoint].position, speed * Time.deltaTime);            
+	}
+}
---
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMove : MonoBehaviour {
+
+    public float moveSpeed;
+    public Transform spawnPosition;
+    public ParticleSystem deathParticles;
+
+    private float maxSpeed = 5f;
+    private Vector3 input;
+    private Rigidbody rbody;
+
+	// Use this for initialization
+	void Start () {
+        rbody = GetComponent<Rigidbody>();
+        transform.position = spawnPosition.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        if(rbody.velocity.magnitude < maxSpeed)
+            rbody.AddForce(input * moveSpeed);
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Goal"))
+            transform.position = spawnPosition.position;
+        else if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        {
+            Instantiate(deathParticles, transform.position, Quaternion.identity);
+            transform.position = spawnPosition.position;
+        }
+            
+
+    }
+}
